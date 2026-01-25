8bd66c4506e2cf34cad081e05bd2c3c276120ca6
Bugaevskii, Aleksandr <800077413@fvtc.edu>
2026-01-25 17:40:04 -0600

    added Welcome div

--- site.js
+++ site.js
@@ -1,1 +1,12 @@
-console.log('TEST')
+const timeText = document.querySelector('#welcome')
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+if (isMorning) {timeText.textContent = "Good morning! Ready to start the day?"}
+if (isAfternoon) {timeText.textContent = "Hey there! Hope your day's going well."}
+if (isEvening) {timeText.textContent = "Good evening! Hope you had a great day."}
+
