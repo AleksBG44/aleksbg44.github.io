d1aef08dfab7a977021e6e642dc297023e4fa310
Bugaevskii, Aleksandr <800077413@fvtc.edu>
2026-02-10 14:48:02 -0600

    Added carousel

--- site.js
+++ site.js
@@ -11,3 +11,42 @@ if (isAfternoon) {timeText.textContent = "Hey there! Hope your day's going well.
 if (isEvening) {timeText.textContent = "Good evening! Hope you had a great day."}
 
 localStorage.setItem("It's a secret to everybody.", "To view the secret joke, please delete System32.")
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+const buttonPrev = document.querySelector('#prev')
+const buttonNext = document.querySelector('#next')
+
+buttonNext.addEventListener('click', () => {
+    currentImage += 1
+    showImages()
+})
+
+buttonPrev.addEventListener('click', () => {
+    currentImage -= 1
+    showImages()
+})
+
+setInterval(() => {
+    currentImage += 1
+    showImages()
+}, 5000);
